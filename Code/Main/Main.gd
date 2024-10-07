52742702811c51f9e258bf1324eb0f48bf8dae53
Skythesiren <skyler.cainhanson@pcc.edu>
2024-10-07 15:51:39 -0700

    Added random spawning position below visible y axis

--- Code/Main/Main.gd
+++ Code/Main/Main.gd
@@ -5,7 +5,7 @@ const BUBBLE = preload("res://Bubble/Bubble.tscn")
 
 func spawnBubble():
 	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(randi_range(0, get_viewport().size.x), 200)
+	bubble_instance.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y + 100)
 	add_child(bubble_instance)
 
 # Called when the node enters the scene tree for the first time.
