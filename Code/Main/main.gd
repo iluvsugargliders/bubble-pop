528f2b1012d4d2083fd6b841af1e6edbd0e62e9c
FrosstyShapron <ace.moroz@pcc.edu>
2024-10-07 16:31:08 -0700

    Bubble making random numbrs

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -2,20 +2,21 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
+@onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	#SpawnBubble()
+	timer.timeout.connect(OnTimedOut)
+	timer.wait_time = 0.25
+
+func OnTimedOut():
+	SpawnBubble()
+
 
 func SpawnBubble():
 	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(500, 600)
-	
-	
+	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
 	add_child(bubble_instance)
 	
 	
-	
-	
-	
-	
-	
-
-func _ready() -> void:
-	SpawnBubble()
