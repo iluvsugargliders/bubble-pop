f3ac12d68498a798e43695345e524074eda7150c
Dreyydoedub <drewaters89@gmail.com>
2024-10-07 15:34:25 -0700

    Spawning and floating bubble.

---
+++ Code/Main/main.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name Main 
+
+const BUBBLE = preload("res://Bubble/bubble.tscn")
+
+func SpawnBubble(): 
+	var bubble_instance = BUBBLE.instantiate()
+	bubble_instance.position = Vector2(100, 200)
+	add_child(bubble_instance)
+func _ready() -> void:
+	SpawnBubble()
