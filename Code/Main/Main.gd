a5b49a858fea68af9f1f8417b00f4013c1a03dc8
Skythesiren <skyler.cainhanson@pcc.edu>
2024-10-07 16:51:43 -0700

    Added different sizes

--- Code/Main/Main.gd
+++ Code/Main/Main.gd
@@ -2,13 +2,31 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
+@onready var timer: Timer = $Timer
 
-func spawnBubble():
-	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y + 100)
-	add_child(bubble_instance)
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
+	#spawnBubble()
+	timer.timeout.connect(OnTimedOut)
+func spawnBubble():
+	var screenLen = randi_range(0, get_viewport().size.x)
+	var spawnHeight = get_viewport().size.y + 100
+	var bubble_instance = BUBBLE.instantiate()
+	
+	bubble_instance.position = Vector2(screenLen, spawnHeight)
+	SizeOfBubble(bubble_instance)
+	add_child(bubble_instance)
+
+func OnTimedOut():
 	spawnBubble()
+
+func SizeOfBubble(bubble_instance):
+	var randNum = randi_range(1,3)
+	
+	if (randNum == 1):
+		bubble_instance.scale = Vector2(0.5, 0.5)
+	elif (randNum == 2):
+		bubble_instance.scale = Vector2(0.333, 0.333)
+	else:
+		bubble_instance.scale = Vector2(0.25, 0.25)
+	
