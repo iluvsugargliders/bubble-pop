732eab040d2ddd4b101cacb95e00689fd17f90d7
striphaux <jay.taylor4@pcc.edu>
2024-10-07 16:30:54 -0700

    Random Bubbles

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -2,17 +2,19 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://bubble/bubble.tscn")
-
-func spawnbubble():
-	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(400, 200)
-	add_child(bubble_instance)
-
-
-
-
-
+@onready var timer: Timer = $Timer
 
 
 func _ready() -> void:
+	timer.timeout.connect(OnTimedOut)
+	timer.wait_time = 2
+
+func OnTimedOut():
 	spawnbubble()
+	#print("Timed Out")
+	
+
+func spawnbubble():
+	var bubble_instance = BUBBLE.instantiate()
+	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
+	add_child(bubble_instance)
