69b41e663156c0d4cbbfb6b40e19ef5c07f2965a
BlackRoseX0X0 <definoreed@gmail.com>
2024-10-07 16:31:02 -0700

    Random Bubbles

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -2,11 +2,16 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	timer.timeout.connect(OnTimeOut)
+	timer.wait_time = 0.25
+
+func OnTimeOut():
+	SpawnBubble()
 
 func SpawnBubble():
 	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(400, 200)
+	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
 	add_child(bubble_instance)
-
-func _ready() -> void:
-	SpawnBubble()
