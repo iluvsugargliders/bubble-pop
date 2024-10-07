8cb9faa05e79d5962dbda1d73b12b00b5238a780
Christopher Gemperle <chris_hg1@hotmail.com>
2024-10-07 16:29:50 -0700

    Set timer for bubble spawning

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -2,7 +2,7 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://Bubble/bubble.tscn")
-var bubbletimer
+@onready var timer: Timer = $Timer
 
 func SpawnBubble():
 	print("Spawning bubble.")
@@ -12,12 +12,11 @@ func SpawnBubble():
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	bubbletimer = Timer.new()
-	bubbletimer.wait_time = 3
-	bubbletimer.start()
-	SpawnBubble()
+	timer.timeout.connect(OnTimedOut)
+	timer.wait_time = 0.25
+	#SpawnBubble()
 
-func _on_timer_timeout():
+func OnTimedOut():
 	var instance = BUBBLE.instantiate()
-	instance.position = Vector2(get_viewport().size.x * randf(), get_viewport().size.y * randf())
+	instance.position = Vector2(randf_range (100, get_viewport().size.x - 100), randf_range(300, get_viewport().size.y))
 	add_child(instance)
