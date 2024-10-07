7fb3984ce84fdeb4d654dddd06c58b1ad66b3bd3
CRMCS <aeson.ranspot@pcc.edu>
2024-10-07 16:29:36 -0700

    random bubble spawning

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -2,17 +2,24 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://Bubble/bubble.tscn")
+@onready var timer: Timer = $Timer
 
-func SpawnBubble():
-	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(600, 300)
-	add_child(bubble_instance)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	SpawnBubble()
+	#SpawnBubble()
+	timer.timeout.connect(OnTimedOut)
+	timer.wait_time = 0.25
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func SpawnBubble():
+	var bubble_instance = BUBBLE.instantiate()
+	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
+	add_child(bubble_instance)
+
+func OnTimedOut():
+	SpawnBubble()
