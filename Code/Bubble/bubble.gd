e55033d93997a39d814d0554a614c2e20bef5c21
iluvsugargliders <rileyhilbers@gmail.com>
2024-10-09 15:45:49 -0700

    popping code

--- Code/Bubble/bubble.gd
+++ Code/Bubble/bubble.gd
@@ -1,7 +1,20 @@
-extends Node2D
+extends Area2D
 class_name Bubble
+
+func _ready() -> void:
+	input_event.connect(OnClicked)
 
 func _process(delta: float) -> void:
 	position.y -= 100 * delta
 	if position.y < 0:
 		queue_free()
+
+func OnClicked(viewport, event, shape_id):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				print("poopy")
+				Pop()
+
+func Pop():
+	queue_free()
